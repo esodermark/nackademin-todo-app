bb7c543414d56e9b3ed328cad75168268622ca4f
esodermark <sodermark@live.se>
2020-09-03 10:21:57 +0200

    added dotenv, created ENVIRONMENT for test, added db.todoLists and db.test_todoLists

--- database/dbConnection.js
+++ database/dbConnection.js
@@ -1,10 +1,23 @@
-var Datastore = require('nedb'), 
+var Datastore = require('nedb')
+require('dotenv').config()
 
 db = {}
-db.todos = new Datastore({ filename: __dirname + "/todos.db" });
-db.users = new Datastore({ filename: __dirname + "/users.db" });
+switch (process.env.ENVIRONMENT) {
+    case 'development':
+        db.todos = new Datastore({ filename: __dirname + "/todos.db" });
+        db.users = new Datastore({ filename: __dirname + "/users.db" });
+        db.todoLists = new Datastore({ filename: __dirname + "/todoLists.db" });
 
-db.todos.loadDatabase();
-db.users.loadDatabase();
+        db.todos.loadDatabase();
+        db.users.loadDatabase();
+        db.todoLists.loadDatabase();
+        break
+    case 'test':
+        db.todoLists = new Datastore({ filename: __dirname + "/test_todoLists.db" });
+
+        db.test_todoLists.loadDatabase();
+        break
+}
+
 
 module.exports = db
