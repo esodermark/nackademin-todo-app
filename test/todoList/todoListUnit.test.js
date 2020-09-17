c9eb00298e0201ff0362b7af9f1822551ab02007
esodermark <sodermark@live.se>
2020-09-17 10:32:49 +0200

    reactivated unit tests

--- test/todoList/todoListUnit.test.js
+++ test/todoList/todoListUnit.test.js
@@ -1,98 +1,98 @@
-// const TodoListModel = require('../../models/TodoListModel')
-// const UserModel = require('../../models/UserModel')
-// const TodoModel = require('../../models/TodoModel')
-// const Database = require('../../database/dbConnection')
+const TodoListModel = require('../../models/TodoListModel')
+const UserModel = require('../../models/UserModel')
+const TodoModel = require('../../models/TodoModel')
+const Database = require('../../database/dbConnection')
 
-// require('chai').should();
-// require('dotenv').config()
+require('chai').should();
+require('dotenv').config()
 
-// const helper = require('./helper')
-
-
-// describe('todoList Unit Tests', () => {
-//     this.currentTest = {}
-
-//     before(async function() {
-//         await Database.connect()
-//     })
-
-//     // after(async function() {
-//     //     await Database.disconnect()
-//     // })
-
-//     beforeEach(async function() {
-//         await UserModel.clear()
-//         await TodoListModel.clear()
-//         // await TodoModel.clear()
-
-//         const user = await helper.generateTestUser()
-//         this.currentTest.user = user
-//     })
+const helper = require('./helper')
 
 
-//     it('should create a todoList', async function() {
-//         const newTodoList = await helper.generateTodoList(this.test.user._id)
+describe('todoList Unit Tests', () => {
+    this.currentTest = {}
 
-//         newTodoList.title.should.equal('Todo List Title')
-//     })
+    before(async function() {
+        await Database.connect()
+    })
+
+    // after(async function() {
+    //     await Database.disconnect()
+    // })
+
+    beforeEach(async function() {
+        await UserModel.clear()
+        await TodoListModel.clear()
+        // await TodoModel.clear()
+
+        const user = await helper.generateTestUser()
+        this.currentTest.user = user
+    })
 
 
-//     it('should get all authorized to read todoLists, role == basic', async function() {
-//         this.test.user.role = 'basic'
-//         await helper.generateTodoList(this.test.user._id)
-//         await helper.generateTodoList(this.test.user._id)
-//         await helper.generateTodoList('unauthorized userId')
+    it('should create a todoList', async function() {
+        const newTodoList = await helper.generateTodoList(this.test.user._id)
 
-//         const todoLists = await TodoListModel.getAllTodoLists()
-//         const authTodoLists = todoLists.authTodos(this.test.user, todoLists.todoLists)
+        newTodoList.title.should.equal('Todo List Title')
+    })
 
-//         todoLists.todoLists.length.should.equal(3)
-//         authTodoLists.length.should.equal(2)
-//     })
+
+    it('should get all authorized to read todoLists, role == basic', async function() {
+        this.test.user.role = 'basic'
+        await helper.generateTodoList(this.test.user._id)
+        await helper.generateTodoList(this.test.user._id)
+        await helper.generateTodoList('unauthorized userId')
+
+        const todoLists = await TodoListModel.getAllTodoLists()
+        const authTodoLists = todoLists.authTodos(this.test.user, todoLists.todoLists)
+
+        todoLists.todoLists.length.should.equal(3)
+        authTodoLists.length.should.equal(2)
+    })
 
     
-//     it('should get all authorized to read todoLists, role == admin', async function() {
-//         await helper.generateTodoList(this.test.user._id)
-//         await helper.generateTodoList(this.test.user._id)
-//         await helper.generateTodoList('unauthorized userId')
+    it('should get all authorized to read todoLists, role == admin', async function() {
+        await helper.generateTodoList(this.test.user._id)
+        await helper.generateTodoList(this.test.user._id)
+        await helper.generateTodoList('unauthorized userId')
 
-//         const todoLists = await TodoListModel.getAllTodoLists()
-//         const authTodoLists = todoLists.authTodos(this.test.user, todoLists.todoLists)
+        const todoLists = await TodoListModel.getAllTodoLists()
+        const authTodoLists = todoLists.authTodos(this.test.user, todoLists.todoLists)
 
-//         todoLists.todoLists.length.should.equal(3)
-//         authTodoLists.length.should.equal(3)
-//     })
+        todoLists.todoLists.length.should.equal(3)
+        authTodoLists.length.should.equal(3)
+    })
     
 
-//     it('should get a todoList by id', async function() {
-//         const newTodoList = await helper.generateTodoList(this.test.user._id)
+    it('should get a todoList by id', async function() {
+        const newTodoList = await helper.generateTodoList(this.test.user._id)
 
-//         const todoList = await TodoListModel.getTodoListById(newTodoList._id)
+        const todoList = await TodoListModel.getTodoListById(newTodoList._id)
 
-//         todoList.todoList.should.eql(newTodoList)
-//         todoList.isOwner(this.test.user).should.equal(true)
-//     })
-
-
-//     it('should update todoList title by id', async function () {
-//         const newTodoList = await helper.generateTodoList(this.test.user._id)
-
-//         const title = 'New Todo List Title'
-//         const id = newTodoList._id
-
-//         const numUpdated = await TodoListModel.updateTodoListTitleById(id, title)
-//         numUpdated.should.equal(1)
-//     })
+        todoList.todoList.should.eql(newTodoList)
+        todoList.isOwner(this.test.user).should.equal(true)
+    })
 
 
-//     it('should delete a todoList by id', async function () {
-//         const newTodoList = await helper.generateTodoList(this.test.user._id)
+    it('should update todoList title by id', async function () {
+        const newTodoList = await helper.generateTodoList(this.test.user._id)
 
-//         const numRemoved = await TodoListModel.deleteTodoListById(newTodoList._id)
+        const title = 'New Todo List Title'
+        const id = newTodoList._id
 
-//         numRemoved.should.equal(1)
-//     })
-// })
+        const numUpdated = await TodoListModel.updateTodoListTitleById(id, title)
+        numUpdated.should.equal(1)
+    })
+
+
+    it('should delete a todoList by id', async function () {
+        const newTodoList = await helper.generateTodoList(this.test.user._id)
+
+        const numRemoved = await TodoListModel.deleteTodoListById(newTodoList._id)
+
+        numRemoved.should.equal(1)
+    })
+})
 
 
 
