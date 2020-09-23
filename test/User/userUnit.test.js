be32aabded3b0b40fe99d72f291427d0f5b84c0a
esodermark <sodermark@live.se>
2020-09-23 15:57:52 +0200

    created unit test for deleteUserById

--- test/User/userUnit.test.js
+++ test/User/userUnit.test.js
@@ -28,4 +28,11 @@ describe('todoList Unit Tests', () => {
         user.username.should.equal(this.test.user.username)
         user.password.should.equal(this.test.user.password)
     })
+
+
+    it('should delete user by id', async function () {
+        const numDeletedUser = await UserModel.deleteUserById(this.test.user._id)
+
+        numDeletedUser.should.equal(1)
+    })
 })
